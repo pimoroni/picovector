d0f08c44f53bf3d5792bea6faac744be975fe33e
Billy T. Pilgrim <tom.b.hallam@gmail.com>
2026-01-05 15:40:38 +0000

    Further rework
    Trying a different tack

--- algorithms/dda.cpp
+++ algorithms/dda.cpp
@@ -4,88 +4,95 @@
 namespace picovector {
 
   void dda(point_t p, point_t v, dda_callback_t cb) {
+    int ix = floor(p.x); // get the top left corner of origin's grid square
+    int iy = floor(p.y);
 
-    // get the current cell of the player (and the current cell of the ray) from their position
-    int playerSquareX = floor(p.x);
-    int playerSquareY = floor(p.y);
-
-    int gx = playerSquareX;
-    int gy = playerSquareY;
-
-    // change the ray angle into vectors
-    // rayDirX = math.cos(rayAngle)
-    // rayDirY = math.sin(rayAngle)
-
-    // establish how many x the ray travels per y, and vice versa
     const float eps = 1e-30f;
-    float angleScaleFactorX = fabs(v.x) > eps ? 1.0f / v.x : 1e30f;
-    float angleScaleFactorY = fabs(v.y) > eps ? 1.0f / v.y : 1e30f;
+    float inv_dx = fabs(v.x) > eps ? 1.0f / v.x : 1e30f; // get y travelled per one move in x
+    float inv_dy = fabs(v.y) > eps ? 1.0f / v.y : 1e30f; // get x travelled per one move in y
 
-    float rayLengthX = 0;
-    float rayLengthY = 0;
+    int step_x = 0 //(v.x > 0.0f) ? 1 : (v.x < 0.0f ? -1 : 0); // get whether we're moving in positive or negative x and y
+    int step_y = 0 //(v.y > 0.0f) ? 1 : (v.y < 0.0f ? -1 : 0);
 
-    float stepX = 1;
-    float stepY = 1;
+    float t_delta_x = fabs(inv_dx); // just the absolute amount of our movement vectors
+    float t_delta_y = fabs(inv_dy);
 
-    // establish the +- xy direction of the ray, and take the first step to the first x and y gridlines
+    float ray_length_x = 0
+    float ray_length_y = 0
+
     if(v.x < 0) {
-      stepX = -1;
-      rayLengthX = (p.x - gx) * angleScaleFactorX;
+        step_x = -1
+        ray_length_x = (p.x - ix) * t_delta_x
     }
     else {
-      stepX = 1;
-      rayLengthX = ((gx + 1) - p.x) * angleScaleFactorX;
+        step_x = 1
+        ray_length_x = ((ix + 1) - p.x) * t_delta_x
     }
 
     if(v.y < 0) {
-      stepY = -1;
-      rayLengthY = (p.y - gy) * angleScaleFactorY;
+        step_y = -1
+        ray_length_y = (p.y - iy) * t_delta_y
     }
     else {
-      stepY = 1;
-      rayLengthY = ((gy + 1) - p.y) * angleScaleFactorY;
+        step_y = 1
+        ray_length_y = ((iy + 1) - p.y) * t_delta_y
     }
 
-    bool stopcast = false;
-    float working_distance = 0;
-    int edge = 0;
+    int i = 0; // don't know why this is here, never referenced again
+    while (true) {
+      float t_exit = std::min(ray_length_x, ray_length_y); // shortest distance to the edge of the current square
 
-    int i = 0;
-    while(true) {
-      bool vertical = false;
+      // calculate the intersection position
+      float hit_x = p.x + v.x * t_exit;
+      float hit_y = p.y + v.y * t_exit;
 
-      // check if the working_distance to the nearest gridline is shorter in x or y,
-      // then use the shorter to populate the intersection orientation.
-      // this happens every step regardless of whether it hits something or not
-      if(rayLengthX < rayLengthY) {
-        gx += stepX;
-        working_distance = rayLengthX;
-        rayLengthX += angleScaleFactorX;
-      }
-      else {
-        gy += stepY;
-        working_distance = rayLengthY;
-        rayLengthY += angleScaleFactorY;
-        vertical = true;
-      }
+      bool dist_to_x_hit = abs(hit_x - round(hit_x))
+      bool dist_to_y_hit = abs(hit_y - round(hit_y))
+
+      // calculate the edge which the intersection occured on (0=top, 1=right, 2=bottom, 3=left)
+      bool vertical = dist_to_y_hit < dist_to_x_hit;
+
+      int edge = 0;
+      float offset = 0;
 
       if(vertical) {
-        edge = (stepX == 1) ? 1 : 3; // step is used to determine if the ray is travelling in the +x or -x direction
+        edge = v.x < 0 ? 3 : 1
+        offset = hit_y - floor(hit_y)
       }
       else {
-        edge = (stepY == 1) ? 0 : 2;
+        edge = v.y < 0 ? 0 : 2
+        offset = hit_x - floor(hit_x)
       }
-
-      float hit_x = p.x + (v.x * working_distance);
-      float hit_y = p.y + (v.y * working_distance);
 
       float distance = sqrt(pow(hit_x - p.x, 2) + pow(hit_y - p.y, 2));
 
-      // calculate the intersection offset
-      float offset = vertical ? (hit_y - floor(hit_y)) : (hit_x - floor(hit_x));
+      // calculate grid square of intersection
+      int gx, gy;
+      if(vertical) {
+        gx = int(hit_x + (edge == 3 ? -0.5f : 0.5f));
+        gy = int(hit_y);
+        gx = floor(hit_x + (v.x > 0.0f ? 0.5f : -0.5f));
+        gy = floor(hit_y);
+      } else {
+        gx = int(hit_x);
+        gy = int(hit_y + (edge == 0 ? -0.5f : 0.5f));
+        gx = floor(hit_x);
+        gy = floor(hit_y + (v.y > 0.0f ? 0.5f : -0.5f));
+      }
 
-      if(!cb(hit_x, hit_y, gx, gy, edge, offset, working_distance)) {
+      if(!cb(hit_x, hit_y, gx, gy, edge, offset, distance)) {
         break;
+      }
+
+      // step to the next cell: whichever boundary we hit first
+      if (ray_length_x < ray_length_y) {
+        ix += step_x;
+        ix = round(ix);
+        ray_length_x += t_delta_x; // next vertical boundary
+      } else {
+        iy += step_y;
+        iy = round(iy);
+        ray_length_y += t_delta_y; // next horizontal boundary
       }
     }
   }
